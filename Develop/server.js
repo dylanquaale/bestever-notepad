1577e693b2577c1e94a3e17ce71e0bf9ca3d0c7e
dylanquaale <dylanquaale33@gmail.com>
2023-01-17 18:04:35 -0600

    running into a file err

--- Develop/server.js
+++ Develop/server.js
@@ -1,56 +1,64 @@
 
-const fs = require('fs');;
-const express = require('express');
-const path = require ('path');
 
+const { Router } = require("express");
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const notes = require("./db/db.json");
 const PORT = 3001;
 const app = express();
-
-app.use(express.static('public'));
-app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 //creates a route that will serve upp the 'public/notes.html page
-app.get("PATH", function(req,res){
-    res.sendFile(__dirname+"FILE PATH")
-})
 
-app.get('/notes'),(req, res) =>
-res.sendFile(path.join(__dirname,'notes.html'));
-
-// `GET *` should return the `index.html` file.
-app.get('*'),(req, res) =>
-res.sendFile(path.join(__dirname,'index.html'));
-
-// Post route for when notes gets created in HTML
-
-app.post('/api/notes', (req, res) => {
-    const newNote = req.body
-    writeToFile(destination, newNote)
-
-    res.json(`${req.method} received`);
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-//fetch request to add a new note
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+// `GET *` should return the `index.html` file.
+app.get("*"), (req, res) => res.sendFile(path.join(__dirname, "index.html"));
 
-const addNote = (note) => {
-    fetch('/api/notes', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify(note),
-    })
-        .then((res) => res.json())
-        .then((notes) => console.log(notes));
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
 
+// Post route for when notes gets created in HTML.
+
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  let response;
+  let id = notes.length + 1;
+  if (req.body) {
+    response = {
+      title: req.body.title,
+      text: req.body.text,
+      id: id,
     };
+    res.status(201).json(response);
+  } else {
+    res.status(400).json("Request body must at least contain a note name");
+  }
+  console.log(req.body);
+  console.log(notes);
+  notes.push(response);
+  fs.writeFileSync("db/db.json", JSON.stringify(notes));
+  res.json(notes);
+  app.delete("/api/notes/:id", (req, res) => {
+    deleteNote(req.params.id, notes);
+    res.json(true);
+  });
+});
 
- //read the `db.json` file and return all saved notes as JSON.
-    app.get('/api/db', (req,res) => res.json(db)); 
+//read the `db.json` file and return all saved notes as JSON.
+app.get("/api/db", (req, res) => res.json(notes));
+app.get("/api/", (req, res) => res.json(notes));
+app.get("/notes", (req, res) => res.json(notes));
 
-    app.listen(PORT, () =>
-    console.log(`App is listening at http://localhost:${PORT}`)
-    );
-
-
-
+app.listen(PORT, () =>
+  console.log(`App is listening at http://localhost:${PORT}`)
+);
